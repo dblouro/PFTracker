8753a2808a42f2357bd6ab82e8d592aeb0a7057e
Ricardo Conceição <75455972+runniOp@users.noreply.github.com>
2024-12-10 16:35:45 +0000

    Add de novos links, alteracoes no template master page, add novos elementos a pag home

---
+++ PFTracker/js/home.js
@@ -0,0 +1,19 @@
+﻿var ctx = document.getElementById('expensePieChart').getContext('2d');
+var expensePieChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Supermercado', 'Automóvel', 'Transporte', 'Lazer'],
+        datasets: [{
+            data: [300, 500, 100, 150],
+            backgroundColor: ['#007bff', '#6610f2', '#28a745', '#ffc107']
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+        }
+    }
+});
