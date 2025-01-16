43d57a89b2a002ef405ee9577501f495a60eb3be
Diogo Louro <Diogo.Louro.T0127776@edu.atec.pt>
2025-01-16 15:13:45 +0000

    fix_design_perfil_utilizador

--- PFTracker/PerfilUtilizador.aspx.designer.cs
+++ PFTracker/PerfilUtilizador.aspx.designer.cs
@@ -78,6 +78,24 @@ namespace PFTracker
         protected global::System.Web.UI.WebControls.TextBox tb_movel;
 
         /// <summary>
+        /// Controle tb_nif.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tb_nif;
+
+        /// <summary>
+        /// Controle tb_morada.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tb_morada;
+
+        /// <summary>
         /// Controle tb_pw.
         /// </summary>
         /// <remarks>
@@ -123,12 +141,12 @@ namespace PFTracker
         protected global::System.Web.UI.WebControls.Label lbl_mensagem;
 
         /// <summary>
-        /// Controle btn_registar.
+        /// Controle btn_guardar.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
         /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btn_registar;
+        protected global::System.Web.UI.WebControls.Button btn_guardar;
     }
 }
